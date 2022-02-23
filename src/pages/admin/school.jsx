dd00488a0ae1a9bc843fbac5fb37e4e1b3d8de30
Julius Muteti <mutetijuliusjr@gmail.com>
2022-02-23 18:48:12 +0300

    Update school.jsx

--- src/pages/admin/school.jsx
+++ src/pages/admin/school.jsx
@@ -17,26 +17,25 @@ import {
   f7,
   View,
 } from 'framework7-react';
-import { useSelector,useDispatch } from 'react-redux';
-import { getSchoolAsync } from '../../redux/schoolSlice';
 
 export default (props) => {
+const [loading, setLoading] = useState(true)
+const [school, setSchool] = useState([])
 
 const getSchool = async () => {
-    const resp = await fetch(`http://localhost:8000/api/schools/${props.id}`);
+    const resp = await fetch(`http://localhost:8000/api/schools/${props.id}`)
     if (resp.ok) {
-        const schl = await resp.json();
-        console.log(schl)
-        return { schl };
+        const schl = await resp.json()
+        setLoading(false)
+        setSchool(schl)
+        //return { schl }
     }
     
 }
 
 useEffect(() => {
-    const school = getSchool()
-    console.log(school)
-  }, [])
-
+  getSchool()
+}, [])
 
 
 
@@ -44,35 +43,45 @@ useEffect(() => {
     
     <Page>
         <Navbar title="School Details" backLink="Back" sliding={false} />
-                  
-        <>
-            <Block >
-                <Menu>
-                    <MenuItem href="#" text="Edit" bgColor="blue" popupOpen="#edit_school" />
-                    <MenuItem href="#" text="Delete" bgColor="red" onClick={()=>{ f7.dialog.confirm('Do You Want To Delete School and Related Entities?', 'Delete School') }} />
-                </Menu>
-                <h3>Description</h3>
-                
-            </Block>
-            <BlockTitle>Department(s)</BlockTitle>
-            <List>
-                <ListItem link="/department/">
-                    Department #1
-                </ListItem>
-                <ListItem link="/department/">
-                    Department #1
-                </ListItem>
-                <ListItem link="/department/">
-                    Department #1
-                </ListItem>
-                <ListItem link="/department/">
-                    Department #1
-                </ListItem>
-                <ListItem link="/department/">
-                    Department #1
-                </ListItem>
-            </List>
-        </> 
+
+        {loading ? (
+            <div className="display-flex justify-content-center padding-top">
+                <Preloader color="gray" size="40px"></Preloader>
+            </div>
+        ):(
+        <div>
+            
+            {school.school != undefined && (
+                <div>
+                    <Block >
+                        <Menu>
+                            <MenuItem href="#" text="Edit" bgColor="blue" popupOpen="#edit_school" />
+                            <MenuItem href="#" text="Delete" bgColor="red" onClick={()=>{ f7.dialog.confirm('Do You Want To Delete School and Related Entities?', 'Delete School') }} />
+                        </Menu>
+                    </Block>
+                    <BlockTitle>School Of {school.school.name}</BlockTitle>
+                    <BlockTitle>Description</BlockTitle>
+                    <Block>{school.school.description}</Block>
+                    <BlockTitle>Department(s)</BlockTitle>
+                        {school.departments != '' ? (
+                            <List>
+                                {school.departments.map((dept)=>{
+                                    <ListItem link="/department/">
+                                        Department #1
+                                    </ListItem>
+                                })}
+                            </List>
+                        ):(
+                           <div>
+                               <Block>There are no departments listed under this school</Block>
+                                <Button outline color="green" text="Add The First Department" />
+                           </div>
+                        )}
+                </div>
+            )}
+        </div> 
+        )}      
+        
     </Page>   
   );
 };
