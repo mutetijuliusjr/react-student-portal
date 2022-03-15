bc533b176fe880b34651a4765aa2c948194e803d
Julius Muteti <mutetijuliusjr@gmail.com>
2022-03-15 14:52:17 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import schoolReducer from './schoolSlice';
 import departmentReducer from './departmentSlice';
 import courseReducer from './courseSlice';
