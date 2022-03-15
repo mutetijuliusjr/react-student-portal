b9865e2232330b6a85dd5bfdf5ca65df9480a0bb
Julius Muteti <mutetijuliusjr@gmail.com>
2022-03-15 13:49:54 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import schoolReducer from './schoolSlice';
 import departmentReducer from './departmentSlice';
+import courseReducer from './courseSlice';
 
 export default configureStore({
 	reducer: {
 		schools: schoolReducer,
 		departments: departmentReducer,
+		courses: courseReducer
 	},
 });
