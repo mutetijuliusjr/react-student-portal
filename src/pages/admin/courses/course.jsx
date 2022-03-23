beecc74d079a419d663b22957ce8f30099a78ae9
Julius Muteti <mutetijuliusjr@gmail.com>
2022-03-23 22:42:57 +0300

    Update course.jsx

--- src/pages/admin/courses/course.jsx
+++ src/pages/admin/courses/course.jsx
@@ -25,44 +25,45 @@ import {
 } from 'framework7-react';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getCoursesAsync } from '../../../redux/courseSlice';
+import { deleteCourseAsync } from '../../../redux/courseSlice';
+import { getUnitsAsync } from '../../../redux/unitSlice';
 
 export default (props) => {
     const { f7router } = props
 
     const dispatch = useDispatch()
     
-    const departments = useSelector((state) => state.departments)
-    const department = departments.find(sch => sch.id == props.id) 
-    const schools = useSelector((state) => state.schools)
     const courses = useSelector((state) => state.courses)
+    const course = courses.find(sch => sch.id == props.id) 
+    const departments = useSelector((state) => state.departments)
+    const units = useSelector((state) => state.units)
     
-    const schoolName = ()=>{
-        if (schools != null) {
-            var schl = schools.find((schl) => schl.id == department.school_id)
-            return `School of ${schl.name}`
+    const departmentName = ()=>{
+        if (departments != null) {
+            var dept = departments.find((dept) => dept.id == course.department_id)
+            return `Department of ${dept.name}`
         }
         else
         {return '...'}
     }
     
-    var departmentCrses = null
+    var courseUnits = null
     
-    if (courses != null) {
-        departmentCrses = courses.filter((schlCrse)=> schlCrse.department_id == department.id)   
+    if (units != null) {
+        courseUnits = units.filter((courseUnit)=> courseUnit.course_id == course.id)   
     }
 
     const deleteToast = f7.toast.create({
         closeTimeout: 5000,
-        text: 'Department Deleted',
+        text: 'Course Deleted',
         position: 'bottom',
     })
     
-    const deleteDepartment = () => {
+    const deleteCourse = () => {
         f7router.back()
         f7.dialog.close()
         f7.dialog.preloader('Loading')
-        dispatch(deleteDepartmentAsync({id: props.id}))
+        dispatch(deleteCourseAsync({id: props.id}))
         setTimeout(() => {
             f7.dialog.close()
             deleteToast.open()
@@ -71,13 +72,13 @@ export default (props) => {
 
     
     useEffect(() => { 
-        dispatch(getCoursesAsync())
+        dispatch(getUnitsAsync())
     }, [dispatch])
 
   return (
     
-    <Page name="department">
-        <Navbar title={`Department of ${department.name}`} backLink="Back" sliding={false} >
+    <Page name="course">
+        <Navbar title={course.name} backLink="Back" sliding={false} >
             <NavRight>
                 <Link popoverOpen=".popover-menu">
                     <Icon>
@@ -88,17 +89,17 @@ export default (props) => {
         </Navbar>
         <Popover className="popover-menu">
             <List noChevron noHairlines>
-                <ListItem link="#" popoverClose title="Edit Department" onClick={()=>f7router.navigate(`/edit-department/${department.id}`)} />
-                <ListItem link="#" popoverClose title="Add Course" />
+                <ListItem link="#" popoverClose title="Edit Course" onClick={()=>f7router.navigate(`/edit-course/${course.id}`)} />
+                <ListItem link="#" popoverClose title="Add Unit" />
                 <ListItem 
                 link="#" 
                 popoverClose
                 textColor="red" 
-                title="Delete Department" 
+                title="Delete Course" 
                 onClick={()=>{ f7.dialog.confirm(
-                    "Do You Want To Delete This Department and It's Related Entities?",
-                    'Delete Department',
-                    ()=>{deleteDepartment()}
+                    "Do You Want To Delete This Course and It's Related Entities?",
+                    'Delete Course',
+                    ()=>{deleteCourse()}
                     )}} />
             </List>
         </Popover>
@@ -106,17 +107,17 @@ export default (props) => {
         <Row noGap>
             <Col width="100" medium="50">
                 <BlockTitle>Name</BlockTitle>
-                <Card outline padding content={department.name} />
+                <Card outline padding content={course.name} />
                 <BlockTitle>Description</BlockTitle>
-                <Card outline padding content={department.description} />
-                <BlockTitle>School</BlockTitle>
+                <Card outline padding content={course.description} />
+                <BlockTitle>Department</BlockTitle>
                 <Card outline className="row padding" >
                     <Col width="70">
-                        <span>{schoolName()}</span>
+                        <span>{departmentName()}</span>
                     </Col>
                     <Col width="30">
                         <Button 
-                        href={`/school/${department.school_id}`}
+                        href={`/department/${course.department_id}`}
                         fill
                         color="blue"
                         text="Manage" 
@@ -125,25 +126,25 @@ export default (props) => {
                 </Card>
             </Col>
             <Col width="100" medium="50">
-                <BlockTitle>Departments</BlockTitle>
-                {courses == null ?
+                <BlockTitle>Courses</BlockTitle>
+                {units == null ?
                     <Block className="display-flex flex-direction-column justify-content-center text-align-center">
                         <div><Preloader className="color-multi" size="24px" text="Loading" /></div>
                     </Block>
                     :
                     <>
-                        {departmentCrses.length == 0 ? 
+                        {courseUnits.length == 0 ? 
                             <Block>
-                                <p>There are no courses for this department</p>
-                                <Button text="Add Department" outline color="green" link="#" />
+                                <p>There are no units for this course</p>
+                                <Button text="Add Unit" outline color="green" link="#" />
                             </Block>
                             :
                             <List inset noHairlines noChevron>
-                                {departmentCrses.map((course)=>
+                                {courseUnits.map((unit)=>
                                     <ListItem 
-                                        key={course.id} 
-                                        title={course.name} 
-                                        link={`/course/${course.id}`} 
+                                        key={unit.id} 
+                                        title={unit.name} 
+                                        link={`/unit/${unit.id}`} 
                                     >
                                         <Icon color="red" slot="media">
                                             <FaGraduationCap />
