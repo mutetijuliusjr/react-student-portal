b65439888b192089718b3a5cb7aafe9a2e8b7e61
Julius Muteti <mutetijuliusjr@gmail.com>
2022-03-25 13:38:21 +0300

    Update semester.jsx

--- src/pages/admin/semesters/semester.jsx
+++ src/pages/admin/semesters/semester.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import {
     FaEllipsisV,
     FaGraduationCap,
-    FaLink
+    FaLink,
+    FaBookOpen
 } from 'react-icons/fa';
 
 import {
@@ -26,43 +27,66 @@ import {
 
 import { useSelector, useDispatch } from 'react-redux';
 import { getCoursesAsync } from '../../../redux/courseSlice';
+import { getUnitsAsync } from '../../../redux/unitSlice';
 
 export default (props) => {
     const { f7router } = props
 
     const dispatch = useDispatch()
     
-    const departments = useSelector((state) => state.departments)
-    const department = departments.find(sch => sch.id == props.id) 
-    const schools = useSelector((state) => state.schools)
+    const semesters = useSelector((state) => state.semesters)
+    const semester = semesters.find(sch => sch.id == props.id) 
     const courses = useSelector((state) => state.courses)
+    const units = useSelector((state) => state.units)
+
+    var u1 = null
+    var u2 = null
+    var u3 = null
+    var u4 = null
+    var u5 = null
+    var u6 = null
+    var u7 = null
+    var u8 = null
+    var u9 = null
+
+    if (units != null) {
+    u1 = units.find((unit)=>unit.id == semester.unit_1)
+    u2 = units.find((unit)=>unit.id == semester.unit_2)
+    u3 = units.find((unit)=>unit.id == semester.unit_3)
+    u4 = units.find((unit)=>unit.id == semester.unit_4)
+    u5 = units.find((unit)=>unit.id == semester.unit_5)
+    u6 = units.find((unit)=>unit.id == semester.unit_6)
+    u7 = units.find((unit)=>unit.id == semester.unit_7)
+    u8 = units.find((unit)=>unit.id == semester.unit_8)
+    u9 = units.find((unit)=>unit.id == semester.unit_9)
+    }
     
-    const schoolName = ()=>{
-        if (schools != null) {
-            var schl = schools.find((schl) => schl.id == department.school_id)
-            return `School of ${schl.name}`
+    const courseName = ()=>{
+        if (courses != null) {
+            var schl = courses.find((schl) => schl.id == semester.course_id)
+            return schl.name
         }
         else
         {return '...'}
     }
     
-    var departmentCrses = null
+    var semesterCrses = null
     
-    if (courses != null) {
-        departmentCrses = courses.filter((schlCrse)=> schlCrse.department_id == department.id)   
+    if (units != null) {
+        semesterCrses = units.filter((schlCrse)=> schlCrse.semester_id == semester.id)   
     }
 
     const deleteToast = f7.toast.create({
         closeTimeout: 5000,
-        text: 'Department Deleted',
+        text: 'Semester Deleted',
         position: 'bottom',
     })
     
-    const deleteDepartment = () => {
+    const deleteSemester = () => {
         f7router.back()
         f7.dialog.close()
         f7.dialog.preloader('Loading')
-        dispatch(deleteDepartmentAsync({id: props.id}))
+        dispatch(deleteSemesterAsync({id: props.id}))
         setTimeout(() => {
             f7.dialog.close()
             deleteToast.open()
@@ -71,13 +95,14 @@ export default (props) => {
 
     
     useEffect(() => { 
+        dispatch(getUnitsAsync())
         dispatch(getCoursesAsync())
     }, [dispatch])
 
   return (
     
-    <Page name="department">
-        <Navbar title={`Department of ${department.name}`} backLink="Back" sliding={false} >
+    <Page name="semester">
+        <Navbar title={`Semester of ${semester.name}`} backLink="Back" sliding={false} >
             <NavRight>
                 <Link popoverOpen=".popover-menu">
                     <Icon>
@@ -88,17 +113,21 @@ export default (props) => {
         </Navbar>
         <Popover className="popover-menu">
             <List noChevron noHairlines>
-                <ListItem link="#" popoverClose title="Edit Department" onClick={()=>f7router.navigate(`/edit-department/${department.id}`)} />
-                <ListItem link="#" popoverClose title="Add Course" />
+                <ListItem link="#" popoverClose 
+                title="Edit Semester" 
+                onClick={()=>f7router.navigate(`/edit-semester/${semester.id}`)} />
+                <ListItem link="#" popoverClose 
+                title="Add Unit" 
+                onClick={()=>f7router.navigate('/new-unit/')} />
                 <ListItem 
                 link="#" 
                 popoverClose
                 textColor="red" 
-                title="Delete Department" 
+                title="Delete Semester" 
                 onClick={()=>{ f7.dialog.confirm(
-                    "Do You Want To Delete This Department and It's Related Entities?",
-                    'Delete Department',
-                    ()=>{deleteDepartment()}
+                    "Do You Want To Delete This Semester and It's Related Entities?",
+                    'Delete Semester',
+                    ()=>{deleteSemester()}
                     )}} />
             </List>
         </Popover>
@@ -106,17 +135,17 @@ export default (props) => {
         <Row noGap>
             <Col width="100" medium="50">
                 <BlockTitle>Name</BlockTitle>
-                <Card outline padding content={department.name} />
-                <BlockTitle>Description</BlockTitle>
-                <Card outline padding content={department.description} />
-                <BlockTitle>School</BlockTitle>
+                <Card outline padding content={semester.name} />
+                <BlockTitle>Code</BlockTitle>
+                <Card outline padding content={semester.code} />
+                <BlockTitle>Course</BlockTitle>
                 <Card outline className="row padding" >
                     <Col width="70">
-                        <span>{schoolName()}</span>
+                        <span>{courseName()}</span>
                     </Col>
                     <Col width="30">
                         <Button 
-                        href={`/school/${department.school_id}`}
+                        href={`/course/${semester.course_id}`}
                         fill
                         color="blue"
                         text="Manage" 
@@ -125,36 +154,99 @@ export default (props) => {
                 </Card>
             </Col>
             <Col width="100" medium="50">
-                <BlockTitle>Departments</BlockTitle>
-                {courses == null ?
-                    <Block className="display-flex flex-direction-column justify-content-center text-align-center">
-                        <div><Preloader className="color-multi" size="24px" text="Loading" /></div>
-                    </Block>
-                    :
-                    <>
-                        {departmentCrses.length == 0 ? 
-                            <Block>
-                                <p>There are no courses for this department</p>
-                                <Button text="Add Department" outline color="green" link="#" />
-                            </Block>
-                            :
-                            <List inset noHairlines noChevron>
-                                {departmentCrses.map((course)=>
-                                    <ListItem 
-                                        key={course.id} 
-                                        title={course.name} 
-                                        link={`/course/${course.id}`} 
-                                    >
-                                        <Icon color="red" slot="media">
-                                            <FaGraduationCap />
-                                        </Icon>
-                                    </ListItem>
-                                )}
-                            </List>
-                        } 
-                    </>
-                    
+                <BlockTitle>Units</BlockTitle>
+                <List inset noHairlines noChevron>
+                {u1 != null &&  
+                    <ListItem
+                        title={u1.name} 
+                        link={`/unit/${u1.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
                 }
+                {u2 != null &&  
+                    <ListItem
+                        title={u2.name} 
+                        link={`/unit/${u2.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u3 != null &&  
+                    <ListItem
+                        title={u3.name} 
+                        link={`/unit/${u3.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u4 != null &&  
+                    <ListItem
+                        title={u4.name} 
+                        link={`/unit/${u4.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u5 != null &&  
+                    <ListItem
+                        title={u5.name} 
+                        link={`/unit/${u5.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u6 != null &&  
+                    <ListItem
+                        title={u6.name} 
+                        link={`/unit/${u6.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u7 != null &&  
+                    <ListItem
+                        title={u7.name} 
+                        link={`/unit/${u7.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u8 != null &&  
+                    <ListItem
+                        title={u8.name} 
+                        link={`/unit/${u8.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                {u9 != null &&  
+                    <ListItem
+                        title={u9.name} 
+                        link={`/unit/${u9.id}`} 
+                    >
+                        <Icon color="green" slot="media">
+                            <FaBookOpen />
+                        </Icon>
+                    </ListItem>
+                }
+                </List>
             </Col>
         </Row>
 
