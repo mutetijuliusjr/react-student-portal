bca2bd8d5c8bd09b14026bc07ebd056d647e5ebf
Julius Muteti <mutetijuliusjr@gmail.com>
2022-03-25 11:46:32 +0300

    Update semesterSlice.js

--- src/redux/semesterSlice.js
+++ src/redux/semesterSlice.js
@@ -1,36 +1,36 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-export const getDepartmentsAsync = createAsyncThunk(
-    'departments/getDepartmentsAsync',
+export const getSemestersAsync = createAsyncThunk(
+    'semesters/getSemestersAsync',
     async () => {
         try {
-            const resp = await fetch('http://localhost:8000/api/departments');
-            const departments = await resp.json();
-            return { departments }; 
+            const resp = await fetch('http://localhost:8000/api/semesters');
+            const semesters = await resp.json();
+            return { semesters }; 
         } 
         catch (error) {
-            const departments = 'error';
-            return { departments }
+            const semesters = 'error';
+            return { semesters }
         }
     }
 );
 
-export const addDepartmentAsync = createAsyncThunk(
-    'departments/addDepartmentAsync',
+export const addSemesterAsync = createAsyncThunk(
+    'semesters/addSemesterAsync',
     async (payload) => {
-        const resp = await fetch('http://localhost:8000/api/departments',{
+        const resp = await fetch('http://localhost:8000/api/semesters',{
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 name: payload.name,
-                description: payload.description,
-                school_id: payload.school_id
+                code: payload.code,
+                course_id: payload.course_id,
             })
         });
 
         if (resp.ok) {
-            const department = await resp.json();
-            return { department };
+            const semester = await resp.json();
+            return { semester };
         }
         else
         {
@@ -40,26 +40,26 @@ export const addDepartmentAsync = createAsyncThunk(
     }
 );
 
-export const editDepartmentAsync = createAsyncThunk(
-    'departments/editDepartmentAsync',
+export const editSemesterAsync = createAsyncThunk(
+    'semesters/editSemesterAsync',
     async (payload) => {
-        const resp = await fetch(`http://localhost:8000/api/departments/${payload.id}`,{
+        const resp = await fetch(`http://localhost:8000/api/semesters/${payload.id}`,{
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload)
         });
 
         if (resp.ok) {
-            const department = await resp.json();
-            return { department };
+            const semester = await resp.json();
+            return { semester };
         }
     }
 ); 
 
-export const deleteDepartmentAsync = createAsyncThunk(
-    'departments/deleteDepartmentAsync',
+export const deleteSemesterAsync = createAsyncThunk(
+    'semesters/deleteSemesterAsync',
     async (payload) => {
-        const resp = await fetch(`http://localhost:8000/api/departments/${payload.id}`,{
+        const resp = await fetch(`http://localhost:8000/api/semesters/${payload.id}`,{
             method: 'DELETE'
         });
 
@@ -69,53 +69,53 @@ export const deleteDepartmentAsync = createAsyncThunk(
     }
 ); 
 
-export const departmentSlice = createSlice({
-    name: 'departments',
+export const semesterSlice = createSlice({
+    name: 'semesters',
     initialState: null,
     reducers:   {
-                    addDepartment: (state, action) => 
+                    addSemester: (state, action) => 
                                 {
                                     
-                                    const newDepartment = 
+                                    const newSemester = 
                                     {
                                         id: payload.id,
                                         name: action.payload.name,
                                         description: payload.description
                                     };
                                     
-                                    return state.push(newDepartment);
+                                    return state.push(newSemester);
                                 },
-                    editDepartment: (state, action) => 
+                    editSemester: (state, action) => 
                                 {
-                                    const index = state.findIndex(department => department.id === action.payload.department.id);
-                                    state[index].name = action.payload.department.name;
-                                    state[index].description = action.payload.department.description;
+                                    const index = state.findIndex(semester => semester.id === action.payload.semester.id);
+                                    state[index].name = action.payload.semester.name;
+                                    state[index].description = action.payload.semester.description;
 
                                 },
-                    deleteDepartment: (state, action) => 
+                    deleteSemester: (state, action) => 
                                 {
-                                    return state.filter((department) => department.id != action.payload.id);
+                                    return state.filter((semester) => semester.id != action.payload.id);
                                 }
                 },
     extraReducers: {
-                    [getDepartmentsAsync.fulfilled]: (state, action) => {
-                        return action.payload.departments;
+                    [getSemestersAsync.fulfilled]: (state, action) => {
+                        return action.payload.semesters;
                     },
-                    [addDepartmentAsync.fulfilled]: (state, action) => {
-                        state.push(action.payload.department);
+                    [addSemesterAsync.fulfilled]: (state, action) => {
+                        state.push(action.payload.semester);
                     },
-                    [editDepartmentAsync.fulfilled]: (state, action) => {
-                        const index = state.findIndex(department => department.id === action.payload.department.id);
-                        state[index].name = action.payload.department.name;
-                        state[index].description = action.payload.department.description;
+                    [editSemesterAsync.fulfilled]: (state, action) => {
+                        const index = state.findIndex(semester => semester.id === action.payload.semester.id);
+                        state[index].name = action.payload.semester.name;
+                        state[index].description = action.payload.semester.description;
                     },
-                    [deleteDepartmentAsync.fulfilled]:  (state, action) => {
-                        return state.filter((department) => department.id != action.payload.id);
+                    [deleteSemesterAsync.fulfilled]:  (state, action) => {
+                        return state.filter((semester) => semester.id != action.payload.id);
                     }
                 }
     
 });
 
-export const { addDepartment, editDepartment, deleteDepartment } = departmentSlice.actions; 
+export const { addSemester, editSemester, deleteSemester } = semesterSlice.actions; 
 
-export default departmentSlice.reducer;
+export default semesterSlice.reducer;
