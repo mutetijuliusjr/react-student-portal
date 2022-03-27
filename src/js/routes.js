1e5a6b024f48849bc2e04c35479a9dc5c902c9af
Julius Muteti <mutetijuliusjr@gmail.com>
2022-03-27 16:12:40 +0300

    Update routes.js

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,7 +2,6 @@
 import HomePage from '../pages/home.jsx';
 import AboutPage from '../pages/about.jsx';
 import FormPage from '../pages/form.jsx';
-//import CatalogPage from '../pages/catalog.jsx';
 import ProductPage from '../pages/product.jsx';
 import SettingsPage from '../pages/settings.jsx';
 
