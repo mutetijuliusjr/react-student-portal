d0b311242230114b5d012df6c9b5bdaddb8fcae5
Julius Muteti <mutetijuliusjr@gmail.com>
2022-04-02 22:46:46 +0300

    Update department.jsx

--- src/pages/admin/departments/department.jsx
+++ src/pages/admin/departments/department.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect} from 'react';
 import {
     FaEllipsisV,
-    FaGraduationCap,
-    FaLink
+    FaBuilding
 } from 'react-icons/fa';
 
 import {
@@ -25,21 +24,27 @@ import {
 } from 'framework7-react';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getDepartmentsAsync } from '../../../redux/departmentSlice';
+import { getDepartmentsAsync, deleteDepartmentAsync } from '../../../redux/departmentSlice';
 
 export default (props) => {
-    const { f7router } = props
-
     const dispatch = useDispatch()
     
-    const departments = useSelector((state) => state.departments)
-    const department = departments.find(sch => sch.id == props.id)
-
+    useEffect(() => { 
+        dispatch(getDepartmentsAsync())
+    }, [dispatch])
+    
+    const { f7router } = props
+    const state = useSelector((state) => state.departments)
+    
+    const departments = state.data
+    const loading = state.loading
+    const error = state.error 
     const deleteToast = f7.toast.create({
         closeTimeout: 5000,
         text: 'Department Deleted',
         position: 'bottom',
     })
+    
     
     const deleteDepartment = () => {
         f7router.back()
@@ -51,16 +56,21 @@ export default (props) => {
             deleteToast.open()
         } ,3000)    
     }
-
     
-    useEffect(() => { 
-        dispatch(getDepartmentsAsync())
-    }, [dispatch])
+    let department
+
+    if (departments.length != 0) {
+        department = departments.find(sch => sch.id == props.id)
+    }
+
 
   return (
     
-    <Page name="department">
-        <Navbar title={`Department of ${department.name}`} backLink="Back" sliding={false} >
+    <Page name="department"> 
+        <Navbar 
+        title={!loading && departments.length != 0 && `Department of ${department.name}`} 
+        backLink="Back" 
+        sliding={false} >
             <NavRight>
                 <Link popoverOpen=".popover-menu">
                     <Icon>
@@ -70,13 +80,10 @@ export default (props) => {
             </NavRight>
         </Navbar>
         <Popover className="popover-menu">
+            {!loading && 
             <List noChevron noHairlines>
-                <ListItem link="#" popoverClose 
-                title="Edit Department" 
-                onClick={()=>f7router.navigate(`/edit-department/${department.id}`)} />
-                <ListItem link="#" popoverClose 
-                title="Add Course" 
-                onClick={()=>f7router.navigate('/new-course/')} />
+                <ListItem link="#" popoverClose title="Edit Department" onClick={()=>f7router.navigate(`/edit-department/${department.id}`)} />
+                <ListItem link="#" popoverClose title="Add Department" onClick={()=>f7router.navigate(`/new-department/?department_id=${department.id}`)} />
                 <ListItem 
                 link="#" 
                 popoverClose
@@ -88,54 +95,81 @@ export default (props) => {
                     ()=>{deleteDepartment()}
                     )}} />
             </List>
+            }
         </Popover>
 
         <Row noGap>
             <Col width="100" medium="50">
                 <BlockTitle>Name</BlockTitle>
-                <Card outline padding content={department.name} />
+                <Card 
+                outline 
+                padding 
+                content={departments.length == 0 ?<p className="skeleton-text">skeleton text</p> :department.name}
+                 />                
                 <BlockTitle>Description</BlockTitle>
-                <Card outline padding content={department.description} />
-                <BlockTitle>School</BlockTitle>
-                <Card outline className="row padding" >
-                    <Col width="70">
-                        <span>School of {department.school.name}</span>
-                    </Col>
-                    <Col width="30">
-                        <Button 
-                        href={`/school/${department.school_id}`}
-                        fill
-                        color="blue"
-                        text="Manage" 
-                        />
-                    </Col>
-                </Card>
+                <Card 
+                outline 
+                padding 
+                content={departments.length == 0 ?
+                        <p className="skeleton-text">
+                            Card with header and footer. 
+                            Card headers are used to display card titles 
+                            and footers for additional information or 
+                            just for custom actions.
+                        </p> 
+                        :
+                        department.description}
+                 />                
             </Col>
+
             <Col width="100" medium="50">
-                <BlockTitle>Courses</BlockTitle>
-                {department.courses.length == 0 ? 
-                    <Block>
-                        <p>There are no courses for this department</p>
-                        <Button text="Add Course" outline color="green" href="/new-course/" />
-                    </Block>
-                    :
-                    <List inset noHairlines noChevron>
-                        {department.courses.map((course)=>
-                            <ListItem 
-                                key={course.id} 
-                                title={course.name} 
-                                link={`/course/${course.id}`} 
-                            >
-                                <Icon color="red" slot="media">
-                                    <FaGraduationCap />
-                                </Icon>
-                            </ListItem>
-                        )}
-                    </List>
-                }                    
+                <BlockTitle>Departments</BlockTitle>
+                {loading && departments.length == 0 ?
+                <Block className="display-flex flex-direction-column justify-content-center text-align-center">
+                    <div><Preloader className="color-multi" size="24px" text="Loading" /></div>
+                </Block>
+                :
+                <>
+                    {departments.length != 0 &&
+                        <>
+                            {department.departments.length == 0 ?
+                            <Block>
+                                <p>There are no departments for this department</p>
+                                <Button text="Add Department" outline color="green" href={`/new-department/?department_id=${department.id}`} />
+                            </Block>
+                            :
+                            <List inset noHairlines='true' noChevron>
+                                {department.departments.map((dept)=>
+                                    <ListItem 
+                                        key={dept.id} 
+                                        title={`Department of ${dept.name}`} 
+                                        link={`/department/${dept.id}`} 
+                                    >
+                                    <Block
+                                        style={{ 
+                                            width: '40px',
+                                            height: '40px',
+                                            borderRadius: '50%',
+                                            margin: '0',
+                                            padding: '8px',
+                                        }}
+                                        bgColor='orange'
+                                        slot="media"
+                                    >
+                                        <FaBuilding style={{fontSize: '24px'}} />
+                                    </Block>
+                                    </ListItem>
+                                )}
+                            </List>
+                            }
+                        </>
+                    }
+                </>
+                }
+                
             </Col>
         </Row>
-
+        
     </Page>  
   );
 };
