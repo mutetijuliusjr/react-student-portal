31ab5baba4dc0b2317d3cbe74b534e7d0651d8ed
Julius Muteti <mutetijuliusjr@gmail.com>
2022-04-11 13:43:35 +0300

    Update new-user.jsx

--- src/pages/admin/users/new-user.jsx
+++ src/pages/admin/users/new-user.jsx
@@ -7,34 +7,33 @@ import {
   List,
   ListInput,
   Block,
+  BlockTitle,
+  ListItem,
 } from 'framework7-react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addDepartmentAsync } from '../../../redux/departmentSlice';
-import { getSchoolsAsync } from '../../../redux/schoolSlice';
+import { getUsersAsync, addUserAsync } from '../../../redux/userSlice';
+import { getRolesAsync } from '../../../redux/roleSlice';
+import { getCoursesAsync } from '../../../redux/courseSlice';
+import { getUnitsAsync } from '../../../redux/unitSlice';
 
-export default (props) => {
+export default () => {
 
     const dispatch = useDispatch()
     
     useEffect(() => {
-        dispatch(getSchoolsAsync())
+        dispatch(getRolesAsync())
+        dispatch(getUsersAsync())
+        dispatch(getCoursesAsync())
+        dispatch(getUnitsAsync())
     }, [dispatch])
     
-    const state = useSelector(state => state.departments)
-    const schools =  useSelector(state => state.schools)
-    const loading = state.loading
-    const error = state.error
-    const updated = state.updated
+    const users =  useSelector(state => state.users)
+    const roles =  useSelector(state => state.roles)
+    const courses =  useSelector(state => state.courses)
+    const units =  useSelector(state => state.units)
 
-    let schoolId
+    const [userRoles, setUserRoles] = useState([])
 
-    const [departmentName, setDepartmentName] = useState('')
-    const [departmentDesc, setDepartmentDesc] = useState('')
-    const [departmentSchl, setDepartmentSchl] = useState('')
-
-    if (props.f7route.query.school_id !== undefined) {
-        schoolId = props.f7route.query.school_id
-    }
 
     const errorNotification = f7.notification.create({
         icon: '<i class="fa fa-exclamation-circle text-color-red"></i>',
@@ -47,109 +46,184 @@ export default (props) => {
     const chooseSchlToast = f7.toast.create({
         icon: '<i class="fa fa-exclamation-circle text-color-green" style="font-size: 30px"></i>',
         title: 'Notice',
-        text: 'Please choose a school.',
+        text: 'Please choose a role.',
         closeTimeout: 3000,
         position: 'center'
     })
 
     const successToast = f7.toast.create({
         icon: '<i class="fa fa-check-circle text-color-green style="font-size: 30px""></i>',
-        text: 'Department has been saved',
+        text: 'User has been saved',
         closeTimeout: 3000,
       })
 
     const onSubmit = (event) => {
         event.preventDefault();
-        if (schoolId == undefined) {
-            if (departmentSchl !== '') {
-                dispatch(
-                    addDepartmentAsync({
-                        name: departmentName,
-                        description: departmentDesc,
-                        school_id: departmentSchl
-                    })
-                )
-            }
-
-            if (departmentSchl == '') {
-                chooseSchlToast.open()
-            }
-        }
-        
-        if (schoolId != undefined) {
             dispatch(
-                addDepartmentAsync({
-                    name: departmentName,
-                    description: departmentDesc,
-                    school_id: schoolId
+                addUserAsync({
+                    name: userName,
+                    description: userDesc,
+                    role_id: roleId
                 })
             )
-        }
     }
         
-    if (updated) {
+    if (users.updated) {
         successToast.open()
     }
     
-    if(error && !updated) {
+    if(users.error && !users.updated) {
         errorNotification.open()
     }
 
     return (
-        <Page name="new-department">
-            <Navbar backLink="Back" sliding  title="Add Department" />
+        <Page name="new-user">
+            <Navbar backLink="Back" sliding  title="Add User" />
 
             <form onSubmit={onSubmit}>
+                <BlockTitle>Personal Information </BlockTitle>
                 <List noHairlinesMd>
                     <ListInput
                         outline
-                        label="Name"
+                        label="First name"
                         floatingLabel
                         type="text"
-                        placeholder="Department name"
+                        placeholder="Required"
                         clearButton
                         required
                         validate
-                        value={departmentName}
-                        onChange={(event) => setDepartmentName(event.target.value)}
                     >
                     </ListInput>
                     <ListInput
                         outline
-                        label="Description"
+                        label="Last name"
                         floatingLabel
-                        type="textarea"
-                        resizable
-                        placeholder="Description"
+                        type="text"
+                        placeholder="Last name"
                         clearButton
-                        value={departmentDesc}
-                        onChange={(event) => setDepartmentDesc(event.target.value)}
                     >
                     </ListInput>
-                    {props.f7route.query.school_id == undefined && schools.data.length != 0 && 
                     <ListInput
                         outline
-                        label="School"
-                        type="select"
-                        value={departmentSchl}
-                        onChange={(event) => setDepartmentSchl(event.target.value)}
-                        placeholder="Please choose..."
+                        label="Surname"
+                        floatingLabel
+                        type="text"
+                        placeholder="Surname"
+                        clearButton
                     >
-                        {schools.data.map((school)=>
-                        <option key={school.id} value={school.id}>{school.name}</option>
-                        )}
-                    </ListInput>
-                    }
+                    </ListInput>                    
+                    <ListInput
+                        outline
+                        label="ID Number"
+                        floatingLabel
+                        type="text"
+                        placeholder="ID Number"
+                        clearButton
+                    >
+                    </ListInput> 
                 </List>
+
+                <BlockTitle>Contact Information</BlockTitle>
+                <List noHairlinesMd>
+                    <ListInput
+                        outline
+                        label="Phone Number"
+                        floatingLabel
+                        type="tel"
+                        placeholder="Phone Number"
+                        clearButton
+                    >
+                    </ListInput>                    
+                    <ListInput
+                        outline
+                        label="E-mail"
+                        floatingLabel
+                        type="email"
+                        placeholder="E-mail"
+                        clearButton
+                    >
+                    </ListInput>  
+                </List>
+
+                <BlockTitle>Roles</BlockTitle>
+                <List noHairlinesMd>
+                    <ListItem title="User Role" />                    
+                    <ListItem title="Please choose..." smartSelect smartSelectParams={{ openIn: 'popover' }}>
+                        <select multiple >
+                            {roles.data.length != 0 && 
+                                <>                            
+                                {roles.data.map((role)=>
+                                <option 
+                                key={role.id} 
+                                value={role.id}
+                                onSelect={(event) => setUserRoles(event.target.value)}
+                                >
+                                {role.name}
+                                </option>
+                                )}
+                                </>
+                            }
+                        </select>
+                    </ListItem>
+                </List>
+                
+                {console.log(userRoles)}
+                <BlockTitle>Student information</BlockTitle>
+                <List noHairlinesMd>
+                    <ListInput
+                        outline
+                        label="Registration number"
+                        floatingLabel
+                        type="text"
+                        placeholder="Required"
+                        clearButton
+                        required
+                        validate
+                    >
+                    </ListInput>
+                    <ListItem title="Student Course(s)" />
+                    <ListItem title="Please choose..." smartSelect smartSelectParams={{ openIn: 'popover' }}>
+                        <select multiple>
+                            {courses.data.length != 0 && 
+                                <>                            
+                                {courses.data.map((course)=>
+                                <option key={course.id} value={course.id}>{course.name}</option>
+                                )}
+                                </>
+                            }
+                        </select>
+                    </ListItem>
+                    <ListInput
+                        label="Intake"
+                        type="date"
+                        placeholder="Intake"
+                    ></ListInput>
+                </List>
+                <BlockTitle>Teacher information</BlockTitle>
+                <List noHairlinesMd>
+                    <ListItem title="Unit(s) Currently Teaching" />
+                    <ListItem title="Please choose..." smartSelect smartSelectParams={{ openIn: 'popover' }}>
+                        <select multiple>
+                            {units.data.length != 0 && 
+                                <>                            
+                                {units.data.map((course)=>
+                                <option key={unit.id} value={unit.id}>{unit.name}</option>
+                                )}
+                                </>
+                            }
+                        </select>
+                    </ListItem>
+                </List>
+
                 <Block>
-                {!schools.loading &&
+                {!roles.loading && 
                     <Button
                     fill
                     round
                     color="green" 
                     text='Save'
-                    loading={loading}
-                    preloader={loading}
+                    loading={users.loading}
+                    preloader={users.loading}
                     type="submit" />
                 }
                 </Block>
